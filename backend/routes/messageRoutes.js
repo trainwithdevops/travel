2d38b5a9bb684694755f030e8612bce08aff166c
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 20:00:05 +0530

    Create messageRoutes.js

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { registerUser, loginUser, getUsers, getUserProfile, updateUserProfile } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/', protect, getUsers);
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
+
+module.exports = router;
