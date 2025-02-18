d07404f3689f00bf74fea34d17ab69150d5d7b23
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 19:52:32 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { registerUser, loginUser, getUsers } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/', getUsers);
+router.get('/', protect, getUsers);
 
 module.exports = router;
