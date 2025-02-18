dab88923d88766e9a50f01f1286602bc2d0b3344
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 19:50:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import ProtectedRoute from './components/ProtectedRoute';
+import FindMembers from './pages/FindMembers';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/contact" component={() => <div>Contact Us</div>} />
         <Route path="/login" component={() => <div>Login</div>} />
         <Route path="/signup" component={() => <div>Signup</div>} />
+        <ProtectedRoute path="/blog" component={() => <div>Blog</div>} />
+        <ProtectedRoute path="/find-members" component={FindMembers} />
       </Switch>
     </Router>
   );
