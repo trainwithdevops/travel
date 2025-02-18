78160bbfe984244e2dfe1c4d7b2a88efc4bf993e
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 19:46:51 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../services/authService';
 
 const Navbar = () => {
+  const { isAuthenticated, logout } = useAuth();
+
   return (
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About Us</Link></li>
         <li><Link to="/contact">Contact Us</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
+        {isAuthenticated ? (
+          <>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/find-members">Find Members</Link></li>
+            <li><button onClick={logout}>Logout</button></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
       </ul>
     </nav>
   );
