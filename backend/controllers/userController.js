d588518cde7df6e4fd6b13f8be6e0b1b5cb3b67e
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 19:35:14 +0530

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+
+exports.registerUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
