e8b79abf86c2e8d8d5594b04ee8e6b46a56e0a3a
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 19:39:39 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={() => <div>About Us</div>} />
+        <Route path="/contact" component={() => <div>Contact Us</div>} />
+        <Route path="/login" component={() => <div>Login</div>} />
+        <Route path="/signup" component={() => <div>Signup</div>} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
