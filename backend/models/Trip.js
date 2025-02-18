ed1a56a2b00620dc45121c74642af7f4a4bacff3
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 20:04:52 +0530

    Create Trip.js

---
+++ backend/models/Trip.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  destination: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+module.exports = Trip;
