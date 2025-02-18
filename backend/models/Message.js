be9d258ae96c8c7e62a7eb0bc1e994eda6c1feea
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 20:04:34 +0530

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,21 +1,26 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
-  destination: String,
-  budget: String,
-  currency: String,
-  language: String,
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const Message = mongoose.model('Message', messageSchema);
 
-module.exports = User;
+module.exports = Message;
