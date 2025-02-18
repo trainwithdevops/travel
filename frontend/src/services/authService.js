bf50c8770cb3c6c9aafc963a03fc66e3dfcec8fd
trainwithdevops <trainwithdevops@gmail.com>
2025-02-18 19:47:09 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import axios from 'axios';
 
 const API_URL = '/api/users/';
 
-const register = (email, password) => {
-  return axios.post(API_URL + 'register', {
-    email,
-    password,
-  });
+const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = async (email, password) => {
+    try {
+      const res = await axios.post(API_URL + 'login', { email, password });
+      localStorage.setItem('token', res.data.token);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+  };
+
+  return { isAuthenticated, login, logout };
 };
 
-export default {
-  register,
-};
+export { useAuth };
