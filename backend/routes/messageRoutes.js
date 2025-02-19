9b810b349cfb9cd947dd4cead6f519bfa777bbf1
trainwithdevops <trainwithdevops@gmail.com>
2025-02-19 15:33:56 +0530

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { sendMessage, getMessages } = require('../controllers/messageController');
 const { protect } = require('../middleware/authMiddleware');
+const csrfProtection = require('../middleware/csrfMiddleware');
 const router = express.Router();
 
-router.post('/', protect, sendMessage);
-router.get('/', protect, getMessages);
+router.post('/', protect, csrfProtection, sendMessage);
+router.get('/', protect, csrfProtection, getMessages);
 
 module.exports = router;
