a39c63034f77dc9d51bdea07b852a85ec6ba06d3
trainwithdevops <trainwithdevops@gmail.com>
2025-02-19 17:18:02 +0530

    Update blogController.js

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -3,7 +3,7 @@ const db = require('../config/db')();
 exports.createBlog = async (req, res) => {
   const { title, content } = req.body;
   try {
-    const [result] = await db.query('INSERT INTO blogs (user_id, title, content) VALUES (?, ?, ?)', [req.user.id, title, content]);
+    await db.query('INSERT INTO blogs (user_id, title, content) VALUES (?, ?, ?)', [req.user.id, title, content]);
     res.status(201).json({ message: 'Blog created successfully' });
   } catch (error) {
     res.status(500).json({ error: error.message });
