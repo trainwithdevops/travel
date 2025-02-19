f311debf1af0e905139a6dd682e7aa5fb2e0a317
trainwithdevops <trainwithdevops@gmail.com>
2025-02-19 15:34:25 +0530

    Update blogRoutes.js

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createBlog, getBlogs } = require('../controllers/blogController');
 const { protect } = require('../middleware/authMiddleware');
+const csrfProtection = require('../middleware/csrfMiddleware');
 const router = express.Router();
 
-router.post('/', protect, createBlog);
-router.get('/', getBlogs);
+router.post('/', protect, csrfProtection, createBlog);
+router.get('/', csrfProtection, getBlogs);
 
 module.exports = router;
