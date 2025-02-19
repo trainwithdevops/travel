72e63c5450e5a73016cd983adccfb7bff11b3c86
trainwithdevops <trainwithdevops@gmail.com>
2025-02-19 15:35:23 +0530

    Update FindMembers.js

--- frontend/src/pages/FindMembers.js
+++ frontend/src/pages/FindMembers.js
@@ -31,51 +31,11 @@ const FindMembers = () => {
   };
 
   return (
-    <div className="find-members">
+    <div className="container">
       <h1>Find Members</h1>
-      <div className="filters">
-        <input
-          type="text"
-          name="destination"
-          placeholder="Destination"
-          value={filters.destination}
-          onChange={handleChange}
-          className="filter-input"
-        />
-        <input
-          type="text"
-          name="budget"
-          placeholder="Budget"
-          value={filters.budget}
-          onChange={handleChange}
-          className="filter-input"
-        />
-        <input
-          type="text"
-          name="currency"
-          placeholder="Currency"
-          value={filters.currency}
-          onChange={handleChange}
-          className="filter-input"
-        />
-        <input
-          type="text"
-          name="language"
-          placeholder="Language"
-          value={filters.language}
-          onChange={handleChange}
-          className="filter-input"
-        />
-      </div>
-      <ul className="members-list">
-        {members.map((member) => (
-          <li key={member._id} className="member-item">
-            {member.email} - {member.destination} - {member.budget} - {member.currency} - {member.language}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default FindMembers;
+      <div className="form-row">
+        <div className="form-group col-md-3">
+          <input
+            type="text"
+            name="destination"
+           
