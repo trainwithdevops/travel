5ae87c22f4464d0b7a038f5f77662adef9adb77e
trainwithdevops <trainwithdevops@gmail.com>
2025-02-20 09:43:13 +0530

    Update tripRoutes.js

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createTrip, getTrips } = require('../controllers/tripController');
+const { createTrip, getTrips, getMap } = require('../controllers/tripController');
 const { protect } = require('../middleware/authMiddleware');
 const csrfProtection = require('../middleware/csrfMiddleware');
 const router = express.Router();
 
 router.post('/', protect, csrfProtection, createTrip);
 router.get('/', protect, csrfProtection, getTrips);
+router.get('/map', protect, csrfProtection, getMap);
 
 module.exports = router;
