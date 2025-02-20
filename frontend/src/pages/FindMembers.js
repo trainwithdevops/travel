d92f13d4151c746a6a49ac22f2c9aca81dbc60c8
trainwithdevops <trainwithdevops@gmail.com>
2025-02-20 09:39:08 +0530

    Update FindMembers.js

--- frontend/src/pages/FindMembers.js
+++ frontend/src/pages/FindMembers.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.css';
 
 const FindMembers = () => {
@@ -14,28 +15,4 @@ const FindMembers = () => {
   useEffect(() => {
     const fetchMembers = async () => {
       const token = localStorage.getItem('token');
-      const res = await axios.get('/api/users', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        params: filters,
-      });
-      setMembers(res.data);
-    };
-
-    fetchMembers();
-  }, [filters]);
-
-  const handleChange = (e) => {
-    setFilters({ ...filters, [e.target.name]: e.target.value });
-  };
-
-  return (
-    <div className="container">
-      <h1>Find Members</h1>
-      <div className="form-row">
-        <div className="form-group col-md-3">
-          <input
-            type="text"
-            name="destination"
-           
+      const res = await axios.get('/api/users
